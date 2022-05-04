a20deb09a943a8b73fc9dda55e6f5bb475732be5
htchan <htchanbc@connect.ust.hk>
2022-05-04 23:51:49 +0800

    add export.go to make it as a public package

---
+++ export.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"io/fs"
+	"fmt"
+	"strings"
+	"path/filepath"
+	"gopkg.in/yaml.v2"
+	"github.com/htchan/ApiParser/internal"
+)
+
+const CONFIG_EXTENSION = ".yaml"
+
+var defaultFormatSet internal.FormatSet
+
+func NewFormatSet(key string, items string, data ...string) internal.FormatSet {
+	return internal.FormatSet{
+		key: internal.Format{
+			Data: data,
+			Items: items,
+		},
+	}
+}
+
+func FromFile(location string) internal.FormatSet {
+	filename := strings.ReplaceAll(filepath.Base(location), CONFIG_EXTENSION, "")
+	var content map[string]*internal.Format
+	f, err := os.Open(location)
+	defer f.Close()
+	if err != nil {
+		return nil
+	}
+	
+	err = yaml.NewDecoder(f).Decode(&content)
+	if err != nil {
+		return nil
+	}
+
+	result := make(internal.FormatSet)
+	for key, item := range content {
+		result[fmt.Sprintf("%v.%v", filename, key)] = *item
+	}
+	return result
+}
+
+func FromDirectory(path string) internal.FormatSet {
+	result := make(internal.FormatSet)
+	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if strings.HasSuffix(info.Name(), CONFIG_EXTENSION) {
+			for key, item := range FromFile(path) {
+				result[key] = item
+			}
+		}
+		return nil
+	})
+	return result
+}
+
+func SetDefault(sets ...internal.FormatSet) {
+	defaultFormatSet = make(internal.FormatSet)
+	for _, set := range(sets) {
+		for key, format := range set {
+			defaultFormatSet[key] = format
+		}
+	}
+}
+
+func Get(key string) internal.Format {
+	return defaultFormatSet.Get(key)
+}
+
+func Parse(key string, content string) internal.Result {
+	return defaultFormatSet.Get(key).Parse(content)
+}
--- internal/format.go
+++ internal/format.go
@@ -1,16 +1,8 @@
 package internal
 
 import (
-	"os"
-	"io/fs"
-	"fmt"
-	"strings"
 	"regexp"
-	"path/filepath"
-	"gopkg.in/yaml.v2"
 )
-
-const CONFIG_EXTENSION = ".yaml"
 
 type Format struct {
 	Data []string `yaml:"data"`
@@ -53,66 +45,4 @@ type FormatSet map[string]Format
 
 func (set FormatSet) Get(key string) Format {
 	return set[key]
-}
-
-var defaultFormatSet FormatSet
-
-func NewFormatSet(key string, items string, data ...string) FormatSet {
-	return FormatSet{
-		key: Format{
-			Data: data,
-			Items: items,
-		},
-	}
-}
-
-func FromFile(location string) FormatSet {
-	filename := strings.ReplaceAll(filepath.Base(location), CONFIG_EXTENSION, "")
-	var content map[string]*Format
-	f, err := os.Open(location)
-	defer f.Close()
-	if err != nil {
-		return nil
-	}
-	
-	err = yaml.NewDecoder(f).Decode(&content)
-	if err != nil {
-		return nil
-	}
-
-	result := make(FormatSet)
-	for key, item := range content {
-		result[fmt.Sprintf("%v.%v", filename, key)] = *item
-	}
-	return result
-}
-
-func FromDirectory(path string) FormatSet {
-	result := make(FormatSet)
-	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
-		if strings.HasSuffix(info.Name(), CONFIG_EXTENSION) {
-			for key, item := range FromFile(path) {
-				result[key] = item
-			}
-		}
-		return nil
-	})
-	return result
-}
-
-func SetDefault(sets ...FormatSet) {
-	defaultFormatSet = make(FormatSet)
-	for _, set := range(sets) {
-		for key, format := range set {
-			defaultFormatSet[key] = format
-		}
-	}
-}
-
-func Get(key string) Format {
-	return defaultFormatSet.Get(key)
-}
-
-func Parse(key string, content string) Result {
-	return defaultFormatSet.Get(key).Parse(content)
 }
